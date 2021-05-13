3af708dd5c0aa0c2eb6e089e45bf48a38f405a18
faivrech <christophe.faivre@natixis.com>
2021-05-13 17:12:35 +0200

    Update controller

--- src/main/java/com/wildcodeschool/doctor/controller/DoctorController.java
+++ src/main/java/com/wildcodeschool/doctor/controller/DoctorController.java
@@ -1,17 +1,29 @@
 package com.wildcodeschool.doctor.controller;
 
 import com.wildcodeschool.doctor.model.Doctor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.server.ResponseStatusException;
 
 @Controller
 public class DoctorController {
 
-    @GetMapping("/doctor/")
+    @GetMapping("/doctor/{number}")
     @ResponseBody
-    public Doctor doctor() {
+    public Doctor doctor(@PathVariable int number) {
+        switch (number) {
+            case 1:
+                return new Doctor(1,"William Hartnell");
+            case 2: case 3: case 4: case 5: case 6: case 7: case 8: case 9: case 10: case 11: case 12:
+                throw new ResponseStatusException(HttpStatus.SEE_OTHER,"L'incarnation "+number+" n'est pas encore disponible.");
+            case 13:
+                return new Doctor(13,"Jodie Whittaker");
+            default:
+                throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Impossible de récupérer l'incarnation "+number+".");
+        }
 
-        return null;
     }
 }
